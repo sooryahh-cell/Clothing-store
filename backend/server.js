cf7114bbcc26f10066a61f483c0921a48060ea08
sooryathejus <sooryathejusk@gmail.com>
2026-05-04 07:04:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-// Routes
+
 app.use('/api/products', require('./routes/products'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/orders', require('./routes/orders'));
